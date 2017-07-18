294f6aa36fcecd35d39b499f739b4f0557ef0cff
Raymond Kam <raymondhmkam@gmail.com>
2017-07-18 08:55:28 -0400

    Done voting view initial layout.

--- FindFoodFastTogether/ViewControllers/VoteViewController.swift
+++ FindFoodFastTogether/ViewControllers/VoteViewController.swift
@@ -12,10 +12,41 @@ class VoteViewController: UIViewController {
     
     var suggestions = [Suggestion]()
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
+}
+
+extension VoteViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "voteCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+}
+
+extension VoteViewController: UICollectionViewDelegate {
+    
+}
+
+extension VoteViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.size.width - 20, height: 324)
+    }
 }
