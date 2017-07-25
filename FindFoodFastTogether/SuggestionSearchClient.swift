5b2f88f92c0576a7ca75416bd2d43f52e446d85d
Raymond Kam <raymondhmkam@gmail.com>
2017-07-25 16:10:51 -0400

    Foundation for abstracting search client which can use any service whether that is Google, Facebook, Yelp, etc. Modified add suggestion view controller to have a search bar and collection view instead of a textfield and button.

---
+++ FindFoodFastTogether/SuggestionSearchClient.swift
@@ -0,0 +1,18 @@
+//
+//  SuggestionSearchClient.swift
+//  FindFoodFastTogether
+//
+//  Created by Raymond Kam on 2017-07-25.
+//  Copyright © 2017 Raymond Kam. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+protocol SuggestionSearchClient {
+    
+    func searchForSuggestions(using query: String,
+                              coordinate: CLLocationCoordinate2D,
+                              radiusInMeters: Int,
+                              completion: @escaping (_ suggestions: [Suggestion]?, _ error: Error?) -> Void)
+}
