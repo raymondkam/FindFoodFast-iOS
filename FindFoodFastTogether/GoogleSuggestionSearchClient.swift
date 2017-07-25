91386c1c341f3c71e0ed12f35573049e4b41b0b1
Raymond Kam <raymondhmkam@gmail.com>
2017-07-25 16:12:27 -0400

    Implemented Google search for establishments within bounded area.

---
+++ FindFoodFastTogether/GoogleSuggestionSearchClient.swift
@@ -0,0 +1,54 @@
+//
+//  GoogleSuggestionSearchClient.swift
+//  FindFoodFastTogether
+//
+//  Created by Raymond Kam on 2017-07-25.
+//  Copyright © 2017 Raymond Kam. All rights reserved.
+//
+
+import Foundation
+import GooglePlaces
+import CoreLocation
+
+class GoogleSuggestionSearchClient: SuggestionSearchClient {
+    
+    private var placesClient = GMSPlacesClient()
+    
+    func coordinateBounds(from center:CLLocationCoordinate2D, meters: Double) -> GMSCoordinateBounds {
+        // 1 unit = 111 km of latitude
+        let offset = meters * MeterInLatitude
+        let latMax = center.latitude + offset
+        let latMin = center.latitude - offset
+        
+        let longOffset = offset * cos(center.latitude * Double.pi / 180.0);
+        let longMax = center.longitude + longOffset
+        let longMin = center.longitude - longOffset
+        let maxCoordinate = CLLocationCoordinate2D(latitude: latMax, longitude: longMin)
+        let minCoordinate = CLLocationCoordinate2D(latitude: latMin, longitude: longMax)
+        
+        return GMSCoordinateBounds(coordinate: maxCoordinate, coordinate: minCoordinate)
+    }
+    
+    func searchForSuggestions(using query: String, coordinate: CLLocationCoordinate2D, radiusInMeters: Int, completion: @escaping ([Suggestion]?, Error?) -> Void) {
+        
+        let filter = GMSAutocompleteFilter()
+        filter.type = .establishment
+        
+        placesClient.autocompleteQuery(query, bounds: coordinateBounds(from: coordinate, meters: 10000), filter: filter) { (predictions, error) in
+            guard error == nil else {
+                print("error occured while doing autocomplete query")
+                return
+            }
+            guard let predictions = predictions else {
+                print("predictions are nil")
+                return
+            }
+            let suggestions = predictions.map({ (prediction) -> Suggestion in
+                return Suggestion(name: prediction.attributedPrimaryText.string, rating: nil)
+            })
+            completion(suggestions, nil)
+        }
+        
+    }
+    
+}
